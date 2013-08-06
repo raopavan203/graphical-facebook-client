ba7fdc8e61906bc7278e27674d7d3b6ff06550b1
root <root@unassigned-hostname.(none)>
2013-08-06 19:07:42 +0530

    With Deployment Script

---
+++ service_layer/Deployment/public_html/setup.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+#
+# Script to install graphical-facebook-client server
+#
+REP=${PWD%/*}
+echo $REP
+sed -i -e 's/install-path/'$REP'/g' ./wsgi/fb.wsgi
+sed -i -e 's/install-path/'$REP'/g' ./fb
+sudo pip install flask
+sudo apt-get install mongodb
+sudo pip install mongoalchemy
+sudo pip install flask-mongoalchemy
+sudo apt-get install apache2
+sudo apt-get install libapache2-mod-wsgi
+sudo cp ./public_html/wsgi/fb.wsgi /etc/apache2/sites-available
+sudo echo "NameVirtualHost *:8081
+Listen 8081" >> /etc/apache2/ports.conf
+a2ensite fb
+/etc/init.d/apache2 reload
---
+++ service_layer/Deployment/public_html/wsgi/fb.wsgi
@@ -0,0 +1,3 @@
+import sys
+sys.path.insert(0,'install-path/public_html/apps/FB')
+from server import app as application
